75cf25c0bebb344d1483e3a8e5283a54e7b287aa
Oleg Ligay <ligayoleg@gmail.com>
2018-08-31 09:58:41 -0400

    update of 08.31.18

--- JS - Understanding the Weird Parts/src/B5_undefined/Starter/app.js
+++ JS - Understanding the Weird Parts/src/B5_undefined/Starter/app.js
@@ -1,4 +1,13 @@
 var a;
 console.log(a);
 
+if(a === undefined){
+	console.log('a is undefined!');
+} else{
+	console.log('a is defined!');
+}
+
+//Never set the value to undefined
+//a = undefined
+
 
---
+++ JS - Understanding the Weird Parts/src/B9_VariableEnvironments/Starter/app.js
@@ -0,0 +1,13 @@
+function b() {
+  var myVar;
+  console.log(myVar);
+}
+
+function a() {
+  let myVar = 2;
+  console.log(myVar);
+}
+
+let myVar = 1;
+console.log(myVar);
+a();
